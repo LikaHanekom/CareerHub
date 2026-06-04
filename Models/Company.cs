2fde8fe0c4bca0b7a7a4212a0689a31efab14e5f
Alika Hanekom <alikahanekom@gmail.com>
2026-06-05 00:09:20 +0200

    Fix: Website to accept null

--- Models/Company.cs
+++ Models/Company.cs
@@ -7,7 +7,7 @@ public class Company
     public Guid Id {get; set;}
     public string Name {get;set;} =string.Empty;
 
-    public string Website {get; set;} = string.Empty;
+    public string? Website { get; set; }
 
     public ICollection<JobListing> JobListings {get; set;} = new List<JobListing>();
 }
